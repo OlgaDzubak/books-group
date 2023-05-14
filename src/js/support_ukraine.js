5976a95eb775f4d6339bef6397fa15c0e0a3c88e
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-05-14 14:26:47 +0300

    Update support_ukraine.js

--- src/js/support_ukraine.js
+++ src/js/support_ukraine.js
@@ -1,40 +1,40 @@
-import { supportItems } from './support_ukraie_items';
+// import { supportItems } from './support_ukraie_items';
 
-const galleryItemsContainer = document.querySelector('.mySwiper');
-const fundsMarkup = createFundsMarkup(supportItems);
+// const galleryItemsContainer = document.querySelector('.mySwiper');
+// const fundsMarkup = createFundsMarkup(supportItems);
 
-galleryItemsContainer.insertAdjacentHTML('beforeend', fundsMarkup);
+// galleryItemsContainer.insertAdjacentHTML('beforeend', fundsMarkup);
 
-// function numberFounds(arr) {
-//   for (let i = 0; i < arr.length; i += 1) {}
+// // function numberFounds(arr) {
+// //   for (let i = 0; i < arr.length; i += 1) {}
+// // }
+// // numberFounds(supportItems);
+
+// function createFundsMarkup(supportItems) {
+//   // console.log(supportItems.children);
+
+//   return supportItems.map(({ title, url, img }) => {
+//       return `
+//             <li class="support_gallery_item swiper-slide">
+//               <a class="support_gallery_link link" href="${url}" title="${title}">
+//                 <svg class="support_gallery_link_svg" height="20">
+//                 <use href="${img}"></use>
+//                 </svg>
+//               </a>
+//             </li>
+//             `;
+//     }).join('');
 // }
-// numberFounds(supportItems);
 
-function createFundsMarkup(supportItems) {
-  // console.log(supportItems.children);
+// // console.log(supportItems);
 
-  return supportItems.map(({ title, url, img }) => {
-      return `
-            <li class="support_gallery_item swiper-slide">
-              <a class="support_gallery_link link" href="${url}" title="${title}">
-                <svg class="support_gallery_link_svg" height="20">
-                <use href="${img}"></use>
-                </svg>
-              </a>
-            </li>
-            `;
-    }).join('');
-}
+// // const swiper = new Swiper('.mySwiper', {
+// //   slidesPerView: 4,
+// //   spaceBetween: 20,
+// //   direction: 'vertical',
+// //   loop: true,
 
-// console.log(supportItems);
-
-// const swiper = new Swiper('.mySwiper', {
-//   slidesPerView: 4,
-//   spaceBetween: 20,
-//   direction: 'vertical',
-//   loop: true,
-
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//   },
-//});
+// //   navigation: {
+// //     nextEl: '.swiper-button-next',
+// //   },
+// //});
